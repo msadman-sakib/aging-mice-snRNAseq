096e45e2a5be53a6e230e9e0480b7587d30042bb
avid_learner <m.sadman.sakib@gmail.com>
2021-07-06 01:43:33 +0200

    Finished code for sctransform, integrating old and young data, using "k.anchor" default value. Generated UMAP plots for old/young conditions, and cell clustering. So far, the data integration looks good, don't need to change "k.anchor = 20"

--- Differential_aging_snRNAseq.R
+++ Differential_aging_snRNAseq.R
@@ -41,25 +41,48 @@ agingnuclei.list  <- lapply(X = agingnuclei.list , FUN = function(x) {
   x <- SCTransform(x, method = "glmGamPoi", verbose = T, vars.to.regress = "percent.mt")
 })
 
-save.image("TempTilsctransformNorm.Rdata")
-##---sofar this worked---####
 
 ##pre-requisites befor integration/anchoring
 features <- SelectIntegrationFeatures(object.list = agingnuclei.list, nfeatures = 3000)
+
 agingnuclei.list <- PrepSCTIntegration(object.list = agingnuclei.list, anchor.features = features)
 
+save.image("TempTilsctransformNorm.Rdata")
+## sofar this worked -----
+
+##for rpca, need to run pca individuall first
+agingnuclei.list <- lapply(X = agingnuclei.list, FUN = function(x) {
+  x <- RunPCA(x, features = features, verbose = F)
+})
+
+
+##---sofar this worked---#### took 1-2 min
 ##finding anchors cell by cell
-agingnuclei.anchors <- FindIntegrationAnchors(object.list = agingnuclei.list, normalization.method = "SCT", anchor.features = features)
-agingnuclei.combined.sct <- IntegrateData(anchorset = agingnuclei.anchors, normalization.method = "SCT")
+agingnuclei.anchors <- FindIntegrationAnchors(object.list = agingnuclei.list, normalization.method = "SCT", anchor.features = features,reduction = "rpca", dims = 1:50) ##This takes forever with the default pca. use rpca for less time. rpca worked nicely! seems like it is working faster than normal "pca" as reduction argument.total time only 2 mins! Also, if the integration doesnt overlap well in UMAP, one can change " k.anchor = 20" in FindIntegrationAnchors function to make them overlapping.
+
+##---sofar this worked---#### took 2 min
+
+agingnuclei.combined.sct <- IntegrateData(anchorset = agingnuclei.anchors, normalization.method = "SCT", verbose = T, dims = 1:50)##adding dims argument in both here and last line seemed to make it work
+
+###comment...so here, due to making the analysis fast, I did the rpca method according to here: https://satijalab.org/seurat/articles/integration_large_datasets.html
+#I only followed the rpca method, did not use the reference method though. In theory according to them, CCA can be superior if, "....the datasets are highly divergent (for example, cross-modality mapping or cross-species mapping), where only a small subset of features can be used to facilitate integration, and you may observe superior results using CCA..."..
+
+##---sofar this worked---#### took 5 minutes.
+
 
 ##clustering
 agingnuclei.combined.sct <- RunPCA(agingnuclei.combined.sct, verbose = T)
-agingnuclei.combined.sct <- RunUMAP(agingnuclei.combined.sct, reduction = "pca", dims = 1:30)
+agingnuclei.combined.sct <- RunUMAP(agingnuclei.combined.sct, reduction = "pca", dims = 1:50)
+
+###need to do these two step for cell clustering numbers
+agingnuclei.combined.sct <- FindNeighbors(agingnuclei.combined.sct, reduction = "pca", dims = 1:50)
+agingnuclei.combined.sct <- FindClusters(agingnuclei.combined.sct, resolution = 0.5)
 
 
 ###plotting
-p1 <- DimPlot(agingnuclei.combined.sct, reduction = "umap", group.by = "stim")
-p2 <- DimPlot(agingnuclei.combined.sct, reduction = "umap", group.by = "seurat_annotations", label = TRUE, repel = TRUE)
+p1 <- DimPlot(agingnuclei.combined.sct, reduction = "umap", group.by = "ident.conditions")
+p2 <- DimPlot(agingnuclei.combined.sct, reduction = "umap", label = TRUE, repel = TRUE)
 p1 + p2
+ggsave("plots/young_old_cell_clusters.pdf", height = 5, width = 10)
 
-
+##---sofar this worked!! got the UMAP plot for old vs young and cell clusters numbers... 
