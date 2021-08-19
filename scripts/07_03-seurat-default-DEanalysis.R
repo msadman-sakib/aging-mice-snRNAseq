c7b36e9538cee033c17f7bef1192924673d8c232
msadman-sakib <m.sadman.sakib@gmail.com>
2021-08-19 13:25:42 +0200

    plotting average expression for two conditions in each cell types

--- scripts/07_03-seurat-default-DEanalysis.R
+++ scripts/07_03-seurat-default-DEanalysis.R
@@ -6,5 +6,29 @@ library(tidyverse)
 # Bring in Seurat object
 seurat = readRDS("Rdata/seurat_integrated_cell_labelled_final.rds")
 
+# let's check average expression between conditions in all cell types. 
+theme_set(theme_cowplot())
+
+##It can be done in a for loop.
+# for(i in levels(Idents(seurat))){
+# subset.cell <- subset(seurat, idents = i)
+# Idents(subset.cell) <- "sample"
+# avg.subset.cell <- log1p(AverageExpression(subset.cell, verbose = T)$RNA)
+# ggplot(as.data.frame(avg.subset.cell), aes(young, old)) + geom_point() + ggtitle(i)
+# ggsave(paste0("DE_analysis_seurat_default/figures/",i,".pdf"),height = 5, width = 5)
+# }
+# 
+
+#But, lets write a function and use map() from purrr to do this. This is wayyyy faster than a for loop.
+avg.expression.plot.fn = function(x){subset.cell <- subset(seurat, idents = x)
+                                  Idents(subset.cell) <- "sample"
+                                  avg.subset.cell <- log1p(AverageExpression(subset.cell, verbose = T)$RNA)
+                                  ggplot(as.data.frame(avg.subset.cell), aes(young, old)) + geom_point() + ggtitle(x)
+                                  ggsave(paste0("DE_analysis_seurat_default/figures/",x,".pdf"),height = 5, width = 5)
+                                  }
+map(levels(Idents(seurat)),avg.expression.plot.fn)
+
+
+
 
 
